a9945b1e7590037398345b9fdf736c802f4759be
Dmitry <kochetov.dmitry@mail.ru>
2022-09-05 12:26:05 +0300

    добавлена блок-схема решения

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] words = { "Sun", "Monday", "Tue",
+      "W", "5", "Friday", "Saturday" };
+
+Console.Clear();
+
+for (int i = 0; i < words.Length; i++)
+{
+    if(words[i].Length <=3)
+    {
+    Console.WriteLine("AS[{0}] = {1}", i, words[i]);
+    }
+}
+Console.ReadKey();
