8d3e2f38ffb8ef3b74e2772450111f828a823293
Dmitry <kochetov.dmitry@mail.ru>
2022-09-05 12:50:35 +0300

    добавлено решение задачи

--- Program.cs
+++ Program.cs
@@ -4,11 +4,24 @@ string[] words = { "Sun", "Monday", "Tue",
 
 Console.Clear();
 
+PrintArray(words);
+
 for (int i = 0; i < words.Length; i++)
 {
     if(words[i].Length <=3)
     {
-    Console.WriteLine("AS[{0}] = {1}", i, words[i]);
+    Console.Write("[{0}] ", words[i]);
     }
 }
 Console.ReadKey();
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < array.Length - 1) Console.Write(array[i] + "; ");
+        else Console.Write(array[i] + "]");
+    }
+    Console.Write(" -> ");
+}
